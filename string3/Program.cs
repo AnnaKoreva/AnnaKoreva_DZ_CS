db9531fc3700aa02b64459a7b1d0c7b76f283a83
Anna_Koreva <koreva.anna@bk.ru>
2022-09-06 18:05:17 +0300

    dz

---
+++ string3/Program.cs
@@ -0,0 +1,12 @@
+﻿//С клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+string str = Console.ReadLine();
+int count = 0;
+for (int i = 0; i<str.Length;i++)
+{
+    if (str[i]!='.') 
+    {
+        count++;
+    }
+    else break;
+}
+System.Console.WriteLine(count);
---
+++ string5/Program.cs
@@ -0,0 +1,12 @@
+﻿//string5. Определить являются ли введенные с клавиатуры символы правильно записью целого числа. 
+//Вычислить сумму цифр введенного числа
+string str = Console.ReadLine();
+int temp = 0;
+int sum = 0;
+if (!Int32.TryParse(str, out temp))
+    Console.WriteLine("Это не является верной записью целого");
+else {
+foreach (char digit in str)
+    sum += digit - 48;
+    Console.WriteLine(sum);
+     }
